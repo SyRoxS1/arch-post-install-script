60c7cc05a5747c777514f72054275d5bece5c733
SyRoxS1 <test.teste44momo@gmail.com>
2025-01-12 16:11:15 +0000

    mullvad integration

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 sudo pacman -Syu
-sudo pacman -S --needed git vscode discord keepassxc unzip ntfs-3g flatpak
+sudo pacman -S --needed git vscode discord keepassxc unzip ntfs-3g flatpak veracrypt base-devel
 
 git config --global user.name "SyRoxS1"
 
@@ -9,9 +9,26 @@ git config --global user.email "$email"
 
 # https://github.com/lutris/docs/blob/master/InstallingDrivers.md intel 32 bits driver
 
-sudo pacman -S --needed lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
+sudo pacman -S --needed --noconfirm lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
 
 mkdir ~/workspace
+
+# Installing mullvad from AUR
+
+cd ~/workspace
+git clone https://aur.archlinux.org/mullvad-vpn-bin.git 
+gpg --recv-key A26581F219C8314C 
+cd mullvad-vpn-bin
+makepkg
+
+
+for file in "$DIR"/mullvad-vpn-bin-*.pkg.tar.zst; do
+  if [[ "$file" != *"-debug"* ]]; then
+    echo "Running file: $file"
+    sudo pacman -U --noconfirm "$file"
+    break
+  fi
+done
 
 # Installing osu (ofc)
 
